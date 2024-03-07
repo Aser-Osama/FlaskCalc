c6953a5acde340185a4d733d8f3a44c743c41b4b
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-03-07 22:41:31 +0200

    Completed.

--- app.py
+++ app.py
@@ -6,20 +6,34 @@ app = Flask(__name__)
 
 @app.get('/')
 def index(name=None):
-    return render_template('index.html', result="")
+    return render_template('index.html')
 
 
 @app.post('/')
 def calculate(name=None):
+    result = ""
     try:     
-        n1 = request.form["number_1"]
-        n2 = request.form["number_2"]
+        num_1 = request.form["number_1"]
+        num_2 = request.form["number_2"]
         operation = request.form['operation']
-    except Exception:
-        return render_template('index.html', result="", error_msg=f"Invalid operation.")
+
+        num_1 = float(num_1)
+        num_2 = float(num_2)
         
-    print(f"{n1}, {n2}, {operation}")
-    return render_template('index.html', result=111)
+        if operation == "a":
+            result = num_1 + num_2
+        elif operation == "s":
+            result = num_1 - num_2
+        elif operation == "m":
+            result = num_1 * num_2
+        elif operation == "d":
+            result = num_1 / num_2
+
+        return render_template('index.html', result=result)
+
+    except Exception as e:
+        return render_template('index.html',
+                               error_msg=f"Invalid operation: {e}")
 
 
 if __name__ == '__main__':
